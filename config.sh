0d0e942172f335fd2de01e039c283167d54d2e55
zzxin107 <972844082@qq.com>
2020-04-14 10:32:12 +0800

    修改poc.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-
+STATUS=""
+while [ "$STATUS" != "200" ]
+do
+    sleep 1
+    STATUS=$(curl -sL -w "%{http_code}" http://web -o /dev/null)
+done
 python3 opt/data/filldata.py http://web 
 echo "config ok"
