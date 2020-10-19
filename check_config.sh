216637d39f4d74f137e7ebc37e8b1a0a70e70a23
zhangzhixin <972844082@qq.com>
2020-10-19 20:49:00 +0800

    删除无用文件

--- check_config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-config_result=`sudo docker-compose logs config`
-
-echo "$config_result"
-
-[[ $config_result =~ "Config Success!" ]] || exit -1
--- check_poc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-config_result=`sudo docker-compose logs poc`
-
-
-[[ $config_result =~ "Poc Success!" ]] || exit -1
