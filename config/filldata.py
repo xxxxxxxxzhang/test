c62a3b8dea86e060088b01ae7b93b1c9d752a2d1
zhangzhixin <972844082@qq.com>
2021-01-24 12:46:43 +0800

    2

--- config/filldata.py
+++ config/filldata.py
@@ -66,30 +66,10 @@ def login_add_user(host,num):
     re = session.post(host + '/admin/new-user', data=user_payload)
     if re.status_code == 200:
         print("Config Success!")
+    else:
+        print("Config Fail")
 
-def add_content(host):
-
-    firefox_opt = webdriver.FirefoxOptions()
-    firefox_opt.add_argument("--headless")
-    driver = webdriver.Firefox(firefox_options=firefox_opt)
-    url =  host+'/admin'
-    driver.get(url)
-    driver.find_element_by_id("jsusername").clear()
-    driver.find_element_by_id("jsusername").send_keys("admin")
-    driver.find_element_by_id("jspassword").clear()
-    driver.find_element_by_id("jspassword").send_keys("111111")
-    driver.find_element_by_xpath(
-        '//div[@class="form-group mt-4"]/button[@class="btn btn-primary btn-lg mr-2 w-100"]').click()  
-    driver.find_element_by_xpath('/html/body/div[3]/div/div[1]/ul/li[4]/a').click()  
-    driver.find_element_by_id("jstitle").clear()
-    driver.find_element_by_id("jstitle").send_keys("hello")
-    driver.find_element_by_id("jsbuttonSave").click()
-
-    driver.find_element_by_xpath('/html/body/div[3]/div/div[1]/ul/li[4]/a').click() 
-
-    driver.find_element_by_id("jstitle").clear()
-    driver.find_element_by_id("jstitle").send_keys("diary")
-    driver.find_element_by_id("jsbuttonSave").click()    
+  
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -101,5 +81,5 @@ if __name__ == '__main__':
     name = ['cuc2', 'cuc3', 'cuc4', 'cuc5']
     for num in name:
         login_add_user(h, num)
-    add_content(h)
+    #add_content(h)
   
